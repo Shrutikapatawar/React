d0802c263729880e5dbccf1602b29efbc2d4177c
Shrutikapatawar <Shrutikapatawar14@gmail.com>
2025-10-04 08:49:30 +0530

    Add files via upload
    There are two types of component the both statement is print using 2 different component.

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Student from './student'
+import Employee from './employee'
+
+function App() {
+  
+
+  return (
+    <>
+     <Student></Student>
+     <Employee></Employee>
+    </>
+  )
+}
+
+export default App
---
+++ Employee.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class employee extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is clls component</h1>
+      </div>
+    )
+  }
+}
+
+export default employee
